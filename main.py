29cad403bcf443db8d480b7d95015d5623faf70e
Alexey Pargachev <pargachev20@gmail.com>
2022-03-10 14:14:51 +0700

    add some commands

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import telebot
+from telebot import types
 
 from config import BOT_TOKEN
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
 
-@bot.message_handler('/start')
+@bot.message_handler(commands=['start'])
 def start(msg):
-    pass
+    bot.send_message(msg.chat.id, f'Привет <b>{msg.from_user.first_name}</b>', parse_mode='html')
+
+
+@bot.message_handler(commands=['site'])
+def get_site(msg):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton('Перейти на сайт университета', url='https://norvuz.ru'))
+    bot.send_message(msg.chat.id, 'Перейти на сайт', reply_markup=markup)
+
+
+bot.polling(none_stop=True)
